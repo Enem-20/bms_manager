e5b4cf4758b438b9bfa6e4da42cf0f75733df6cd
Sergey Zamaro <firewall606@gmail.com>
2025-06-28 12:06:07 +0300

    add instruction and installation script

---
+++ README.md
@@ -0,0 +1,6 @@
+# bms_manager
+<p>После записи образа выполните:</p>
+<p>cd ~/catkin_ws/src</p>
+<p>git clone https://github.com/Enem-20/bms_manager.git</p>
+<p>cd bms_manager</p>
+<p>sudo bash ./rosinstall.sh</p>
---
+++ rosinstall.sh
@@ -0,0 +1,12 @@
+apt-get install ros-noetic-serial                                                   -y
+cd ~/catkin_ws
+
+echo MAKE
+source /opt/ros/noetic/setup.bash
+catkin_make
+
+cp -f /home/ubuntu/catkin_ws/src/bms_manager/bms_manager.service /etc/systemd/system/bms_manager.service
+
+systemctl daemon-reload
+systemctl enable bms_manager.service
+systemctl start bms_manager.service
