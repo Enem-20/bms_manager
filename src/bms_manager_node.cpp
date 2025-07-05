502284babfb014152fea220b687a39c191db3eba
Sergey Zamaro <firewall606@gmail.com>
2025-07-05 15:02:18 +0300

    fix node handle type

--- src/bms_manager_node.cpp
+++ src/bms_manager_node.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
         std::lock_guard<std::mutex> lock(bms_mutex);
         BMSFactory::scanForBMS(bmses, "/dev", nh);
     }
-    MavToPublisher::getInstance(nh);
+    MavToPublisher::getInstance(&nh);
     ros::Subscriber rc_sub = nh.subscribe("/mavros/rc/in", 10, rc_callback);
     ros::Timer bms_check_timer = nh.createTimer(ros::Duration(5.0), checkBMSPorts);
 
